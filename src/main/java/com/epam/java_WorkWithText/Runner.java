2893dac84a829d362bfbddc2c07bc85d1d62633b
Dmitry <Valevach.90@mail.ru>
2021-08-14 18:37:13 +0300

    14.08.2021

--- src/main/java/com/epam/java_WorkWithText/Runner.java
+++ src/main/java/com/epam/java_WorkWithText/Runner.java
@@ -1,12 +1,14 @@
 package com.epam.java_WorkWithText;
 
-import com.epam.java_WorkWithText.io.ReadTextFromFile;
+
+import com.epam.java_WorkWithText.io.WorkWithFile;
 
 import java.io.File;
 
 public class Runner {
     public static void main(String[] args) {
-        ReadTextFromFile read = new ReadTextFromFile();
-        System.out.println( read.readingFromFile("./src/main/java/com/epam/java_WorkWithText/source/test1.txt"));
+        WorkWithFile workWithFile = new WorkWithFile();
+        System.out.println(workWithFile.fromFile("myText.txt"));
+
     }
 }
