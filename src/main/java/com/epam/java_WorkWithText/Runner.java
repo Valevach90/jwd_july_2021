45d9ff61aded3002f58dc958263536b7a4ba40ad
Ivatich <valevach2311@mail.ru>
2021-08-14 10:09:10 +0300

    14.08.2021

--- src/main/java/com/epam/java_WorkWithText/Runner.java
+++ src/main/java/com/epam/java_WorkWithText/Runner.java
@@ -1,4 +1,12 @@
 package com.epam.java_WorkWithText;
 
+import com.epam.java_WorkWithText.io.ReadTextFromFile;
+
+import java.io.File;
+
 public class Runner {
+    public static void main(String[] args) {
+        ReadTextFromFile read = new ReadTextFromFile();
+        System.out.println( read.readingFromFile("./src/main/java/com/epam/java_WorkWithText/source/test1.txt"));
+    }
 }
